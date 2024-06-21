4558b10c56e5a641e2fef531130862f3b9255517
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-21 16:22:32 +0100

    feat: changes

--- packages/ui-react/__tests__/ui-react.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const uiReact = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(uiReact(), 'Hello from uiReact');
-console.info('uiReact tests passed');
--- packages/ui-react/lib/ui-react.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = uiReact;
-
-function uiReact() {
-  return 'Hello from uiReact';
-}
