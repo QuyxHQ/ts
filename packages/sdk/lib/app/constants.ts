915af4662f613b345ba07082ca6664c56874ee09
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-21 23:57:51 +0100

    feat: fixed endpoint typo

--- packages/sdk/lib/app/constants.ts
+++ packages/sdk/lib/app/constants.ts
@@ -1,7 +1,7 @@
 import { createAsyncLocalStorage } from './async.storage'
 import { isBrowser } from './helper'
 
-const ENDPOINT_URL = 'https://api.quyx/xyz'
+const ENDPOINT_URL = 'https://api.quyx.xyz'
 const STORAGE = isBrowser() ? createAsyncLocalStorage('storage') : undefined
 
 export default { ENDPOINT_URL, STORAGE }
